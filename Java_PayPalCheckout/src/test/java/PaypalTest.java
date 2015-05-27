86ff7e531a8fa6d103beae39e7abde600f3673f9
steve.sturtevant <steve.sturtevant@appdynamics.com>
2015-05-27 17:45:34 -0500

    adding unit tests

---
+++ Java_PayPalCheckout/src/test/java/PaypalTest.java
@@ -0,0 +1,38 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaypalTest {
+
+	@Test
+	public void Test() {
+		assertNotNull("Test Pass");
+	}
+
+	@Test
+	public void Test1() {
+		assertNotNull("Test Pass");
+	}
+	
+	@Test
+	public void Test2() {
+		assertNotNull("Test Pass");
+	}
+	
+	@Test
+	public void Test3() {
+		assertNotNull("Test Pass");
+	}
+	
+	@Test
+	public void Test4() {
+		assertNotNull("Test Pass");
+	}
+	
+	@Test
+	public void Test5() {
+		assertNotNull("Test Pass");
+	}
+}
