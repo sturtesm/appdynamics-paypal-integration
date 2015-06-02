034de1460dffccc9692b36b630264090d4dc3c6a
steve.sturtevant <steve.sturtevant@appdynamics.com>
2015-06-01 22:40:08 -0500

    fixing client pool bug

--- Java_PayPalCheckout/src/main/java/com/appdynamics/sample/rest/client/WebClientPoolHelper.java
+++ Java_PayPalCheckout/src/main/java/com/appdynamics/sample/rest/client/WebClientPoolHelper.java
@@ -12,49 +12,49 @@ public class WebClientPoolHelper {
 	private static final int RETRY_FREQ_SECS = 1;
 
 	private Hashtable<String, WebClientPool> clientPool = new Hashtable<String, WebClientPool> ();
-	
-    Logger logger = Logger.getLogger(WebClientPoolHelper.class);
 
-	
-    private static WebClientPoolHelper helper = null;
-    
+	Logger logger = Logger.getLogger(WebClientPoolHelper.class);
+
+
+	private static WebClientPoolHelper helper = null;
+
 	private WebClientPoolHelper() {
-		
+
 	}
-	
+
 	public static WebClientPoolHelper getInstance() {
 		if (helper == null) {
 			helper = new WebClientPoolHelper();
 		}
 		return helper;
 	}
-	
+
 	private String generateHostTargetKey(String host, String targetUrl) {
 		return host + ":" + targetUrl;
 	}
-	
+
 	public WebClient getWebClient(String host, String targetUrl, boolean retry, int timeoutSecs) {
-		
+
 		assert (host != null);
 		assert (targetUrl != null);
-		
+
 		String hostTargetKey = generateHostTargetKey(host, targetUrl);
-		
+
 		WebClientPool pool = clientPool.get(hostTargetKey);
-		
+
 		if (pool == null) {
 			logger.info("Creating a new web client pool for " + hostTargetKey);
-			
+
 			pool = new WebClientPool(host, targetUrl);
-			
+
 			clientPool.put(hostTargetKey, pool);
 		}
 		else {
 			logger.info("Got existing pool (" + pool + ") from cache for " + hostTargetKey);
 		}
-		
+
 		logger.info("WebClient Pool Cache Size: " + clientPool.size());
-		
+
 		return getWebClient(pool, retry, timeoutSecs);
 	}
 
@@ -62,18 +62,18 @@ public class WebClientPoolHelper {
 			int timeoutSecs) 
 	{
 		logger.debug("Attempting to get Web Client From Pool, timoutSecs left=" + timeoutSecs);
-		
+
 		WebClient client = pool.getWebClient();
-		
+
 		if (client == null && retry && timeoutSecs > 0) {
-			
+
 			logger.warn("Unable to get WebClient from pool (" + pool.getHost() + "/" + pool.getPath() + ") ");
 			logger.warn("Trying again for up to " + timeoutSecs + " seconds");
-			
+
 			try {
 				Thread.sleep(RETRY_FREQ_SECS * 1000);
 			} catch (Exception e) {
-				
+
 			}
 			client = getWebClient(pool, retry, timeoutSecs - RETRY_FREQ_SECS);
 		}
@@ -89,23 +89,23 @@ public class WebClientPoolHelper {
 		else {
 			logger.debug("Got WebClient from pool (" + pool.getHost() + "/" + pool.getPath() + ") ");
 		}
-		
+
 		logger.debug("Current pool usage is [" + pool.getUsageCount() + " / " + pool.getSize() + "] ");
 
-		
+
 		return client;
 	}
-	
+
 	public void returnWebClient(String host, String targetUrl, WebClient client) {
 		String hostTargetKey = generateHostTargetKey(host, targetUrl);
 
 		logger.info("Attempting to return WebClient to pool: " + hostTargetKey);
-		
+
 		WebClientPool pool = clientPool.get(hostTargetKey);
-		
+
 		if (pool != null && client != null) {
 			pool.returnWebCient(client);
-			
+
 			logger.info("Returnning web client for pool " + hostTargetKey + ", pool usage is now [ " + 
 					pool.getUsageCount() + " / " + pool.getSize() + " ]");
 		}
@@ -120,14 +120,14 @@ public class WebClientPoolHelper {
 		String hostTargetKey = generateHostTargetKey(host, path);
 
 		logger.info("Attempting to return WebClient to pool: " + hostTargetKey);
-		
+
 		WebClientPool pool = clientPool.get(hostTargetKey);
-		
+
 		if (pool != null) {
 			pool.seedPool();
+
+			logger.info("Resetting web client for pool " + hostTargetKey + ", pool usage is now [ " + 
+					pool.getUsageCount() + " / " + pool.getSize() + " ]");
 		}
-		
-		logger.info("Resetting web client for pool " + hostTargetKey + ", pool usage is now [ " + 
-				pool.getUsageCount() + " / " + pool.getSize() + " ]");
 	}
 }
