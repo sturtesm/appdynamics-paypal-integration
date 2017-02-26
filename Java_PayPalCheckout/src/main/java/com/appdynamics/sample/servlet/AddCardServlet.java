82946519bd3f5fee38a368ec984146e42172298f
Steve Sturtevant <steve.sturtevant@appdynamics.com>
2017-02-26 05:00:01 +0000

    \applying step1\

--- Java_PayPalCheckout/src/main/java/com/appdynamics/sample/servlet/AddCardServlet.java
+++ Java_PayPalCheckout/src/main/java/com/appdynamics/sample/servlet/AddCardServlet.java
@@ -56,81 +56,9 @@ public class AddCardServlet extends PaypalDemoServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String authToken = null;
-		String cardInfo = null;
-
-		String doAbortParam = req.getParameter("abort");
-		String resetPool = req.getParameter("reset");
-
-		boolean reset = (resetPool != null && resetPool.trim().length() > 0);
-		boolean abort = (doAbortParam == null || doAbortParam.trim().length() == 0);
-
-		logger.info("Processing payment request, abort == " + abort);
-
-		try {
-			
-			if (reset) {
-				resetAuthWebClientPool();
-			}
-			else {
-				String userID = (abort) ? null : "Mr. Rogers";
-				authToken = callAuthService((doAbortParam != null), userID);
-
-				cardInfo = addCard(authToken);
-			}
-		} catch (InvalidCardException e) {
-			logger.fatal("Handling invalid card format exception");
-			e.printStackTrace();
-
-			throw new ServletException(e);
-		} catch (Exception e) {
-			e.printStackTrace();
-
-			throw new ServletException(e);
-		}
-
-		logger.info("Successfully processed payment request");
-
 		ResultPrinter.addResult(req, resp, "Added Credit Card", 
-				"Visa - Auth Token (" + authToken + ")", cardInfo, null);
+				"Stub", "Add Card Stub", null);
 		
 		req.getRequestDispatcher("jsp/response.jsp").forward(req, resp);
-	}
-
-
-	/**
-	 * adds a new credit card, actually calls a mid-tier service which then calls to paypal.
-	 * 
-	 * If authToken == null then simulates an error by throwing an InvalidCardFormatException
-	 * 
-	 * @param authToken
-	 * @return
-	 * @throws InvalidCardException 
-	 */
-	private String addCard(String authToken) throws InvalidCardException {
-		String host = "http://localhost:7090";
-		String service = "/service/v1/paypal/card/credit/create/" + 
-				authToken + "/4111111111111111/visa";
-
-		/**
-    	WebClient client = 
-    			clientHelper.getWebClient(host, service, true, 10);
-		 */
-		WebClient client = 	
-				WebClient.create(host).path(service);
-
-		if (client == null) {
-			logger.fatal("Failed to create web client to invoke payment service");
-
-			return null;
-		}
-		else {
-			logger.info("Successfully got web client from pool for [ " + host + " : " + service + " ]");
-		}
-
-		client.type(MediaType.TEXT_PLAIN);
-		client.accept("text/plain", "text/html");
-
-		return client.get(String.class);
 	}
 }
