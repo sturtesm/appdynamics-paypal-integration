11644cc14aa1c9d7e6576d9c6bc7dbd792c70a9b
Steve Sturtevant <steve.sturtevant@appdynamics.com>
2018-06-15 08:00:05 +0000

    \applying step2\

--- Java_PayPalCheckout/src/main/java/com/appdynamics/sample/servlet/CheckoutServlet.java
+++ Java_PayPalCheckout/src/main/java/com/appdynamics/sample/servlet/CheckoutServlet.java
@@ -17,7 +17,6 @@
 package com.appdynamics.sample.servlet;
 
 import java.io.IOException;
-
 import java.io.PrintWriter;
 import java.util.logging.Level;
 
@@ -62,15 +61,91 @@ public class CheckoutServlet extends PaypalDemoServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		getPaymentDetails();
-		
+		String authToken = null;
+		String paymentInfo = null;
+
+		String doAbortParam = req.getParameter("abort");
+		String resetPool = req.getParameter("reset");
+
+		boolean reset = (resetPool != null && resetPool.trim().length() > 0);
+		boolean abort = (doAbortParam == null || doAbortParam.trim().length() == 0);
+
+		logger.info("Processing payment request, abort == " + abort);
+
+		try {
+			
+			getPaymentDetails();
+			
+			if (reset) {
+				resetAuthWebClientPool();
+			}
+			else {
+				String userID = (abort) ? null : "Mr. Rogers";
+				authToken = callAuthService((doAbortParam != null), userID);
+
+				paymentInfo = initiatePayment(authToken);
+			}
+		} catch (InvalidCardException e) {
+			logger.fatal("Handling invalid card format exception");
+			e.printStackTrace();
+
+			throw new ServletException(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			throw new ServletException(e);
+		}
+
+		logger.info("Successfully processed payment request");
+
 		ResultPrinter.addResult(req, resp, "Credit Card Payment", 
-				"Stub", "Credit Card Payment Stub", null);
+				"Auth Token (paymentInfo)", paymentInfo, null);
 		
 		req.getRequestDispatcher("jsp/response.jsp").forward(req, resp);
 	}
-	
+
+
+	/**
+	 * initiate a payment, actually calls a mid-tier service which then calls to paypal.
+	 * 
+	 * If authToken == null then simulates an error by throwing an InvalidCardFormatException
+	 * 
+	 * @param authToken
+	 * @return
+	 * @throws InvalidCardException 
+	 */
+	private String initiatePayment(String authToken) throws InvalidCardException {
+		String host = "http://localhost:7090";
+		String service = "/service/v1/paypal/payment/";
+
+		/**
+    	WebClient client = 
+    			clientHelper.getWebClient(host, service, true, 10);
+		 */
+		WebClient client = 
+				WebClient.create("http://localhost:7090").path(
+						"/service/v1/paypal/payment/credit/create/" + authToken);
+
+		if (client == null) {
+			logger.fatal("Failed to create web client to invoke payment service");
+
+			return null;
+		}
+		else {
+			logger.info("Successfully got web client from pool for [ " + host + " : " + service + " ]");
+		}
+
+		/** when we throw the exception we won't put the client back into the pool */
+		if (authToken == null) {
+			throw new InvalidCardException ("Invalid Auth Token Exception, Payment Auth Token != null");
+		}
+
+		client.type(MediaType.TEXT_PLAIN);
+		client.accept("text/plain", "text/html");
+
+		return client.get(String.class);
+	}
+
 	private PaymentCard getPaymentDetails() {
 		return PaymentCardInfo.instance().getCard();
 	}
